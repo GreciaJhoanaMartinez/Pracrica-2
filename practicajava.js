879fa4356905b57df8171c0914ee2b2f87ee885f
GreciaJhoanaMartinez <greciajhoanamartinezcontreras@gmail.com>
2023-04-20 11:58:22 -0600

    first commit

---
+++ practicajava.js
@@ -0,0 +1,20 @@
+var x = "Bienvenido juan";
+var y = "we\'re the champions";
+
+console.log(x);
+console.log(y);
+
+var a=12;
+var b= 3;
+var c= 13;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a *b);
+console.log(a/b);
+
+console.log(a + b * c);
+console.log((a + b) * c);
+console.log(b/a*c);
+console.log(b/(a*c));
+console.log(b-a+c);
